005172967d03b4397921f3f89db8bf605105acbb
Ali Osman Öztürk <github@aliosmanozturk.me>
2025-04-28 13:46:51 +0300

    Create Blank Project

---
+++ SwiftUI-Supabase-Official-Tutorial/SwiftUI-Supabase-Official-Tutorial/SwiftUI_Supabase_Official_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Supabase_Official_TutorialApp.swift
+//  SwiftUI-Supabase-Official-Tutorial
+//
+//  Created by Ali Osman Öztürk on 28.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Supabase_Official_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
